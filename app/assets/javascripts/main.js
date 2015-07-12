568bac42ae67b2dd7e54b127b94f85127bee13af
Ke Min Roy Chan <RoyChan@RCs-MacBook-Pro.local>
2015-07-12 04:53:52 -0700

    working good

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,170 +1,220 @@
 // ===================================================================================================  
 // Variable declaration
 // =================================================================================================== 
-var USE_MOUSE = false;
-var USE_VR = true;
+var USE_MOUSE = true;
+var USE_VR = false;
 var CSS_ALPHA = 0.6;
 
 var g_mapScaleFactor = 400;
 
 var table = [
-    ["Hello", "Hihi", "1.00794", 1, 1],
-    ["Hello", "Hihi", "4.002602", 18, 1],
-    ["Hello", "Hihi", "6.941", 1, 2],
-    ["Hello", "Hihi", "9.012182", 2, 2],
-    ["Hello", "Hihi", "10.811", 13, 2],
-    ["Hello", "Hihi", "12.0107", 14, 2],
-    ["Hello", "Hihi", "14.0067", 15, 2],
-    ["Hello", "Hihi", "15.9994", 16, 2],
-    ["Hello", "Hihi", "18.9984032", 17, 2],
-    ["Hello", "Hihi", "20.1797", 18, 2],
-    ["Hello", "Hihi", "22.98976...", 1, 3],
-    ["Hello", "Hihi", "24.305", 2, 3],
-    ["Hello", "Hihi", "26.9815386", 13, 3],
-    ["Hello", "Hihi", "28.0855", 14, 3],
-    ["Hello", "Hihi", "30.973762", 15, 3],
-    ["Hello", "Hihi", "32.065", 16, 3],
-    ["Hello", "Hihi", "35.453", 17, 3],
-    ["Hello", "Hihi", "39.948", 18, 3],
-    ["Hello", "Hihi", "39.948", 1, 4],
-    ["Hello", "Hihi", "39.948", 1, 4],
-    ["Hello", "Hihi", "22.98976...", 1, 3],
-    ["Hello", "Hihi", "24.305", 2, 3],
-    ["Hello", "Hihi", "26.9815386", 13, 3],
-    ["Hello", "Hihi", "28.0855", 14, 3],
-    ["Hello", "Hihi", "30.973762", 15, 3],
-    ["Hello", "Hihi", "32.065", 16, 3],
-    ["Hello", "Hihi", "35.453", 17, 3],
-    ["Hello", "Hihi", "39.948", 18, 3],
-    ["Hello", "Hihi", "39.948", 1, 4],
-    ["Hello", "Hihi", "39.948", 1, 4],
+    ["hello", "1"],
+    ["IT's SG50!!", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
 
-    ["H", "Hydrogen", "1.00794", 1, 1],
-    ["He", "Helium", "4.002602", 18, 1],
-    ["Li", "Lithium", "6.941", 1, 2],
-    ["Be", "Beryllium", "9.012182", 2, 2],
-    ["B", "Boron", "10.811", 13, 2],
-    ["C", "Carbon", "12.0107", 14, 2],
-    ["N", "Nitrogen", "14.0067", 15, 2],
-    ["O", "Oxygen", "15.9994", 16, 2],
-    ["F", "Fluorine", "18.9984032", 17, 2],
-    ["Ne", "Neon", "20.1797", 18, 2],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
 
-    ["H", "Hydrogen", "1.00794", 1, 1],
-    ["He", "Helium", "4.002602", 18, 1],
-    ["Li", "Lithium", "6.941", 1, 2],
-    ["Be", "Beryllium", "9.012182", 2, 2],
-    ["B", "Boron", "10.811", 13, 2],
-    ["C", "Carbon", "12.0107", 14, 2],
-    ["N", "Nitrogen", "14.0067", 15, 2],
-    ["O", "Oxygen", "15.9994", 16, 2],
-    ["F", "Fluorine", "18.9984032", 17, 2],
-    ["Ne", "Neon", "20.1797", 18, 2],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
 
-    ["H", "Hydrogen", "1.00794", 1, 1],
-    ["He", "Helium", "4.002602", 18, 1],
-    ["Li", "Lithium", "6.941", 1, 2],
-    ["Be", "Beryllium", "9.012182", 2, 2],
-    ["B", "Boron", "10.811", 13, 2],
-    ["C", "Carbon", "12.0107", 14, 2],
-    ["N", "Nitrogen", "14.0067", 15, 2],
-    ["O", "Oxygen", "15.9994", 16, 2],
-    ["F", "Fluorine", "18.9984032", 17, 2],
-    ["Ne", "Neon", "20.1797", 18, 2],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
 
-    ["H", "Hydrogen", "1.00794", 1, 1],
-    ["He", "Helium", "4.002602", 18, 1],
-    ["Li", "Lithium", "6.941", 1, 2],
-    ["Be", "Beryllium", "9.012182", 2, 2],
-    ["B", "Boron", "10.811", 13, 2],
-    ["C", "Carbon", "12.0107", 14, 2],
-    ["N", "Nitrogen", "14.0067", 15, 2],
-    ["O", "Oxygen", "15.9994", 16, 2],
-    ["F", "Fluorine", "18.9984032", 17, 2],
-    ["Ne", "Neon", "20.1797", 18, 2],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["Na", "Sodium", "22.98976...", 1, 3],
-    ["Mg", "Magnesium", "24.305", 2, 3],
-    ["Al", "Aluminium", "26.9815386", 13, 3],
-    ["Si", "Silicon", "28.0855", 14, 3],
-    ["P", "Phosphorus", "30.973762", 15, 3],
-    ["S", "Sulfur", "32.065", 16, 3],
-    ["Cl", "Chlorine", "35.453", 17, 3],
-    ["Ar", "Argon", "39.948", 18, 3],
-    ["K", "Potassium", "39.948", 1, 4],
-    ["K", "Potassium", "39.948", 1, 4],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
 
 ];
 
+//-----------------Ajax call to retrieve voices
+
+var jsonwindow = document.getElementById("jsonWindow");
+var rawjsonwindow = document.getElementById("rawJsonWindow");
+//ajax call to get json
+$(document).ready(function() {
+    // run the first time; all subsequent calls will take care of themselves
+    setTimeout(worker, 1000);
+});
+
+var highestID = 0;
+//var queue = [];
+function worker() {
+    $.ajax({
+        url: '/getVoices/',
+        success: function(data,status, jqXHR) {
+
+            //alert(data.length);
+            //alert(jqXHR.responseText);
+            //rawjsonwindow.innerHTML = jqXHR.responseText;
+
+            for (i = 0; i < data.length ; i ++){
+                //table[i][0] = data[i].text;
+                //console.log(data[i].text);
+                //table[i][1] = data[i].id;
+                if (data[i].id > highestID){
+
+                    //queue.push(data[i]);
+                    AddsceneCSSElementsIndividual(data[i].id,data[i].text);
+                    highestID = data[i].id;
+                };
+            }
+
+            //alert(queue.length);
+            //jsonwindow.innerHTML = "";
+            //for (i = 0 ; i< queue.length ; i++ ) {
+            //    jsonwindow.innerHTML += "ID=" + queue[i].id + " Text="+ queue[i].text + "<br> "  ;
+            //}
+
+            //$('#div1').html(parsedJson.count);
+        },
+        complete: function() {
+            // Schedule the next request when the current one's complete
+            setTimeout(worker, 5000);
+        }
+    });
+};
+
+
+//--------------
 var camera;
 
 // WebGL variables
@@ -207,7 +257,7 @@ function Init()
 	
 	// Add the camera
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 50000);
-    camera.position.set(0 * g_mapScaleFactor, 0 * g_mapScaleFactor, 4 * g_mapScaleFactor);
+    camera.position.set(0 * g_mapScaleFactor, 2.8 * g_mapScaleFactor, 9 * g_mapScaleFactor);
 	
 	// Create the WebGL Renderer
 	rendererGL = new THREE.WebGLRenderer();
@@ -231,11 +281,11 @@ function Init()
 	
 	// Add sceneGL elements
     AddsceneGLElements();
-	AddsceneCSSElements();
+	//AddsceneCSSElements();
 	  
 	// Add axes
-	axes = buildAxes( 1000 );
-	sceneGL.add( axes );
+	//axes = buildAxes( 1000 );
+	//sceneGL.add( axes );
 	
 	// Variables for picking things inside the virtual environment
 	raycaster = new THREE.Raycaster();
@@ -251,7 +301,7 @@ function Init()
 	{
 	    controls = new THREE.OrbitControls(camera);
 	    //controls = new THREE.TrackballControls( camera );
-	    controls.target = new THREE.Vector3(0 * g_mapScaleFactor, 0 * g_mapScaleFactor, 0 * g_mapScaleFactor);
+	    controls.target = new THREE.Vector3(0 * g_mapScaleFactor, 2.8 * g_mapScaleFactor, 0 * g_mapScaleFactor);
 	    controls.zoomSpeed = 0.3;
         
 	}
@@ -471,9 +521,15 @@ function CheckCommentObjectTransparency()
 
         if (t_render == false) {
             t_object.element.style.backgroundColor = 'rgba(255,255,255,' + (0.0) + ')';
+            t_object.element.children[0].style.color = 'rgba(255,255,255,' + (0.0) + ')';
+            t_object.element.children[1].style.color = 'rgba(255,255,255,' + (0.0) + ')';
+            t_object.element.style.border = 'none';
         }
         else {
             t_object.element.style.backgroundColor = 'rgba(255,255,255,' + (CSS_ALPHA) + ')';
+            t_object.element.children[0].style.color = 'rgba(255,255,255,' + (CSS_ALPHA) + ')';
+            t_object.element.children[1].style.color = 'rgba(255,255,255,' + (CSS_ALPHA) + ')';
+            t_object.element.style.border = '5px solid rgba(255,255,255,1)';
         }
 
     }
@@ -521,6 +577,7 @@ function RotateCommentObjects()
             t_object.position.x = (3000 - m_layerNum * 300) * Math.sin(2 * Math.PI * g_time * t_dir + phi);
             t_object.position.y = 1000 + m_layerNum * 400;
             t_object.position.z = (3000 - m_layerNum * 300) * Math.cos(2 * Math.PI * g_time * t_dir + phi);
+
         }
         else
         {
@@ -708,6 +765,38 @@ function AddLights()
 }
 
 // ===================================================================================================
+function AddsceneCSSElementsIndividual(a_id, a_text)
+{
+
+        var element = document.createElement( 'div' );
+        element.className = 'element';
+        element.style.backgroundColor = 'rgba(255,255,255,' + (CSS_ALPHA) + ')';
+        element.style.width = "200px";
+        element.style.height = "200px";
+
+        var symbol = document.createElement( 'div' );
+        symbol.className = 'symbol';
+        symbol.textContent = a_id;
+        element.appendChild( symbol );
+
+        var details = document.createElement( 'div' );
+        details.className = 'details';
+        details.innerHTML = a_text;
+        element.appendChild( details );
+
+        var object = new THREE.CSS3DObject( element );
+        object.position.x = 0;
+        object.position.y = 0;
+        object.position.z = 0;
+        sceneCSS.add( object );
+
+        m_cssObjects.push(object);
+
+        element.parent = object;
+        object.element.onclick = function () { GetCSSElementInfo(this); };
+}
+
+// ===================================================================================================
 function AddsceneCSSElements()
 {
 	for ( var i = 0; i < table.length; i ++ ) 
@@ -721,10 +810,10 @@ function AddsceneCSSElements()
 		element.style.width = "200px";
 		element.style.height = "200px";
 
-		var number = document.createElement( 'div' );
-		number.className = 'number';
-		number.textContent = i + 1;
-		element.appendChild( number );
+		//var number = document.createElement( 'div' );
+		//number.className = 'number';
+		//number.textContent = i + 1;
+		//element.appendChild( number );
 
 		var symbol = document.createElement( 'div' );
 		symbol.className = 'symbol';
